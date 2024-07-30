cf4d20fe777fee8a7c3eed599cf28e35fe699b3c
bangnd <nguyendangbanghn@gmail.com>
2024-07-30 10:08:07 +0700

    log env

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,29 +1,30 @@
+import env from "@/env";
 import useAuthStore from "@/stores/authStore";
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:1337",
-  timeout: 10000,
+    baseURL: env.API_URL,
+    timeout: 10000,
 });
 
 http.interceptors.request.use(
-  function (config) {
-    const token = useAuthStore.getState().token;
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
+    function (config) {
+        const token = useAuthStore.getState().token;
+        if (token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
 );
 
 http.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
+    function (response) {
+        return response;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
 );
 
 export default http;
--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,6 @@
 const env = {
     API_URL: import.meta.env.API_URL || "http://localhost:1337",
 };
+console.log("🚀 ~ env:", env)
 
 export default env;
